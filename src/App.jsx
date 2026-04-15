c4013d8dc46b569873aa465892a5568067d1d051
Rahul Soni <rs1626521@gmail.com>
2026-04-15 18:41:41 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,3 @@
 // Paste your Achievers Club website component code here
 // (Use the full component from your existing canvas website)
-export { default } from '../achievers_club_business_website.
+export { default } from '../achievers_club_business_website.jsx'
